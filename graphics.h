22f4ccfd8b2ada3d965a43fb7eb81ccd66555083
thrye333 <61040578+thrye333@users.noreply.github.com>
2025-03-14 14:31:04 -0700

    Create graphics.h

---
+++ graphics.h
@@ -0,0 +1,26 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+class ParticleGraphics {
+	Color color(100,10,150);
+	public:
+	ParticleGraphics(Color c) {
+		color = c;
+	}
+	void setColor(Color c) {
+		color = c;
+	}
+	Color getColor() {
+		return color;
+	}
+	//takes point coords
+	void drawPoint(int x, int y) {}
+	//takes upper left point, width, and height
+	void drawRect(int x, int y, int w, int h) {}
+	//takes center point, width, and height
+	void drawOval(int x, int y, int w, int h) {}
+	//takes a vector of coords ({x1,y1,x2,y2,x3,y3,...})
+	void drawPolygon(vector<int> vertices) {}
+	//takes two endpoints
+	void drawLine(int x1, int y1, int x2, int y2) {}
+}
