148f94893fdb00f555691a032d147c4a20601910
GurshanSingh <0944145@my.scccd.edu>
2025-03-14 14:53:40 -0700

    made particleSystem.cc

---
+++ particleSystem.cc
@@ -0,0 +1,13 @@
+#include <iostream> //For debugging via cout
+#include "linkedlist.h"
+#include "particle.h"
+
+
+int main(){
+
+std::cout << "this code ran from particleSystems.cc" << std::endl;
+
+return 0;
+}
+
+
