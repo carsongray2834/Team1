82058a80afe7273e889cbbf7bbb23602db81e208
thrye333 <61040578+thrye333@users.noreply.github.com>
2025-03-14 10:21:36 -0700

    Update particle.h
    Created a start to the Particle class. Still needs a constructor and methods.

--- particle.h
+++ particle.h
@@ -1,1 +1,12 @@
+#ifndef PART_H
+#define PART_H
 
+//we can probably move the Data struct into this file, since this class will probably take it instead of Node. 
+
+class Particle {
+	//Data data; holding off on this for now
+	char shape;
+	int lifeFramesLeft;
+	Color c(100,10,150);//feel free to change the default color
+}
+#endif
