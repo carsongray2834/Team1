4640352077a93e0d2fe1c70a66334e9d6ef9cecb
Gurshan Singh <105119199+Mcall555@users.noreply.github.com>
2025-03-19 01:30:27 -0700

    Update particleSystem.h
    typo fixes

--- particleSystem.h
+++ particleSystem.h
@@ -23,7 +23,7 @@ public:
 	void moveParticles() {}
 	void add(Particle p) {particles.push_back(p);}
 	void drawParticles() {}
-	std::list returnPartiiclesList(){return particles;}
+	std::list returnParticlesList(){return particles;}
 
 	//if this is your key I wouldn't put it here because it it public to github now
 //>>>>>>> 1db294e4ef3ac28ddb9d3369a2e60c3b6909e52b
