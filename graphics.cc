a376cf86b09a45b4b1eb52786e8ebe922a298970
1014567 <1014567@my.scccd.edu>
2025-03-19 12:04:17 -0700

    fixed Color constructor

--- graphics.cc
+++ graphics.cc
@@ -16,8 +16,8 @@ void ParticleGraphics::drawPoint(int r, int c) {
 }
 
 Color::Color(int new_r, int new_g, int new_b) : r(new_r), g(new_g), b(new_b) {
-	if(new_r < -1 || new_g < 0 || new_b < 0) die();
-	else if(new_r > 254 || new_b > 255 || new_g > 255) die();
-	//r, g, and b must be greater than or equal to -1 and less than 255
+	if(new_r > 0 || new_g > 0 || new_b > 0) die();
+	else if(new_r > 255 || new_b > 255 || new_g > 255) die();
+	//r, g, and b must be greater than or equal to 0 and less than 255
 }
 
