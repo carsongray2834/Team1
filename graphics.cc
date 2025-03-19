475ac448b8a63199956ce356f9f545e6e34afe19
Lucas Riley <61040578+thrye333@users.noreply.github.com>
2025-03-18 21:37:21 -0700

    Update graphics.cc
    Changing drawPoint() parameters to be better named.

--- graphics.cc
+++ graphics.cc
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-void ParticleGraphics::drawPoint(int x, int y) {
-	movecursor(y,x);
+void ParticleGraphics::drawPoint(int r, int c) {
+	movecursor(r,c);
 	setbgcolor(color.r,color.g,color.b);
 	cout << " " << flush;
 }
