3bed81a85cd6595e3be32d1f5fc301409e042a64
Lucas Riley <0987350@my.scccd.edu>
2025-03-21 14:19:57 -0700

    stubbed scene3()

--- riley.h
+++ riley.h
@@ -1,1 +1,16 @@
+#include "particleSystem.h"
+
+void scene3() {
+        //we'll need:
+        //      a polygonal light,
+        //      a line connecting it to the top of the screen,
+        //      a spark timer releasing ballistic particles from the center of the bottom edge of the light
+        //      something to calculate the vertex positions of the pendulum light
+        //for now:
+        //      draw a rectangle
+        //      then release one round of sparks
+        //      then make lines and polygons
+        //      then set the timer
+        //      then make it move
+}
 
