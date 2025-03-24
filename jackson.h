cf7f22515a21ff21032dea6d430393f0ef269389
1014567 <1014567@my.scccd.edu>
2025-03-23 19:56:57 -0700

    updating

--- jackson.h
+++ jackson.h
@@ -17,13 +17,14 @@ void waterfall() {
 	//pa.set_rows(rows + 10);
 	pa.set_rows(rows + 10); //I am trying to keep the rectangle
 	for(int i = 0; i < 50; i++) { //make 50 waters lol
-		Particle p(cyan, BALLISTIC, 1, 1, rand() % 15 + 1, 1, 1440);
-		p.set_ay(2); //set gravity to 4m/s (cause it's water)
+		Particle p(cyan, BALLISTIC, 15, 11, rand() % 20 + 1, 1, 1440);
+	//	p.set_ay(2); //set gravity to 4m/s (cause it's water)
 		pa.add(p);
 	}
 	for(int j = 0; j < 100; j++) {
 		pa.moveAndDraw();
 		usleep(250'000); // 4 fps
+	//	pa.drawRect(15,0 , 10, rows - 20);	
 	}
 	
 }	
