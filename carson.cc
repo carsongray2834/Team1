75814251a3a7b3e9359ff4be50df835273e89d77
carsongray <carsongray2834@gmail.com>
2025-03-25 21:41:14 -0700

    carson.cc

--- carson.cc
+++ carson.cc
@@ -4,40 +4,89 @@
 #include <unordered_map>
 #include "carson.h"
 
-static_mass::static_mass(int nx, int ny, int nmass) {
-		x = nx;
-		y = ny;
-		if (nmass > 10) {nmass = 10;}
-		if (nmass < 1) {nmass = 1;}
-		mass = nmass;
-	}
-void static_mass::map_accel(int rows, int cols) {
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
-
-				accelMap[std::pair(j, i)] = std::pair(1.0, 2.0);
-			}
-		}
-	}
-
 void carsoneffect() {
 	//Prepares to render particles
-//	std::srand(time(0));
+	std::srand(time(0));
 	auto [rows, cols] = get_terminal_size();
 	rows--;
 	cols--;
-	ParticleSystem ps(rows,cols);
+	cParticleSystem ps(rows, cols);
 	clearscreen();
 	show_cursor(false);
 	
+	ps.add(cParticle(2, 2, 1, 1));
+	ps.add(cParticle(2, 3, 1, 1));
+	ps.add(cParticle(2, 4, 1, 1));
+	//ps.add(cParticle(2, 5, 1, 1));
+	//ps.add(cParticle(2, 6, 1, 1));
+	ps.moveAndDraw();
+	ps.moveAndDraw();
+
 	//Main loop for drawing particles
-	for (int i = 0; i < 100; i++) {
-		
-		ps.moveAndDraw();
-		//usleep(500'000);
-	}
 	clearscreen();
 	resetcolor();
 	show_cursor(true);
 	movecursor(0, 0);
 }
+
+static_mass::static_mass(int nx, int ny, int nmass) {
+	x = nx;
+	y = ny;
+	if (nmass > 10) {nmass = 10;}
+	if (nmass < 1) {nmass = 1;}
+	mass = nmass;
+}
+void static_mass::map_accel(int rows, int cols) {
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+
+			accelMap[std::pair(j, i)] = std::pair(1.0, 2.0);
+		}
+	}
+}
+
+
+
+//my classes methods definitions
+
+void cParticle::draw() {
+	movecursor(y, x);
+	setcolor(c.r, c.g, c.b);
+	if (ch == ' ') setbgcolor(c.r, c.g, c.b);
+	std::cout << ch;
+	resetcolor();
+}
+
+void cParticle::hide(int w, int k) {
+	movecursor(w, k);
+	setbgcolor(1, 1, 1);
+	std::cout << ' ';
+	resetcolor();
+}
+
+void cParticle::move() {
+	dx += ax;
+	dy += ay;
+	x += dx;
+	y += dy;
+}
+
+void cParticle::hmd() {
+	int tx = x;
+	int ty = y;
+	move();
+	hide(ty, tx);
+	draw();
+	std::cout.flush();
+}
+
+
+void cParticleSystem::add(cParticle p) {
+	particles.add(p);
+}
+void cParticleSystem::moveAndDraw(){
+	for (cNode<cParticle>* temp = particles.get_head(); temp != nullptr; temp = temp->next) {
+		temp->data.hmd();
+	}
+}
+
--- carson.h
+++ carson.h
@@ -1,6 +1,65 @@
 #ifndef CARSON_H
 #define CARSON_H
 #include <map>
+#include "linkedlist.h"
+
+template <typename T>
+struct cNode {
+	cNode* next = nullptr;
+	cNode* prev = nullptr;
+	T data;
+};
+
+template <typename T>
+class cList {
+	cNode<T>* head;
+	cNode<T>* tail;
+	int size;
+public:
+	cNode<T>* get_head() {return head;}
+	int get_size() {return size;}
+	cList() {
+		head = nullptr;
+		tail = nullptr;
+		size = 0;
+	}
+	~cList() {
+		while (size > 0) dlNode(head);
+	}
+
+
+	//add node
+	void add(T d) {
+		if (!head) {
+			head = new cNode<T>;
+			head->data = d;
+			tail = head;
+		}
+		else {
+			cNode<T>* temp = new cNode<T>;
+			temp->data = d;
+			temp->prev = tail;
+			tail->next = temp;
+		}
+		size++;
+	}
+	//delete node
+	void dlNode(cNode<T>* curr) {
+		if (size == 0) return;
+		else if (curr == nullptr) return;
+		else if (size == 1) {
+			delete head;
+			size = 0;
+			head = nullptr;
+			tail = head;
+		}
+		else if (curr == head) {
+			head = head->next;
+			delete head->prev;
+			head->prev = nullptr;
+		}
+	}
+};
 
 class static_mass {
 	int x;
@@ -13,6 +72,40 @@ public:
 	int get_y() {return y;}
 	int get_mass() {return mass;}
 	void map_accel(int rows, int cols);
+};
+
+class cParticle {
+	double x;
+	double y;
+	double dx;
+	double dy;
+	double ax;
+	double ay;
+	char ch;
+	Color c;
+public:
+	cParticle(double nx = 0, double ny = 0, double ndx = 0, double ndy = 0, double nax = 0, double nay = 0, char nch = '*', Color nc = Color{0, 255, 0}) : x(nx), y(ny), dx(ndx), dy(ndy), ax(nax), ay(nay), ch(nch), c(nc) {}
+	void draw();
+	void hide(int w, int k);
+	void move();
+	//hide move draw
+	void hmd();
+};
+
+class cParticleSystem {
+	int cols;
+	int rows;
+	cList<cParticle> particles;
+public:
+	cParticleSystem(int nrows, int ncols) {
+		cols = ncols;
+		rows = nrows;
+	}
+	void add(cParticle p);
+	void moveAndDraw();
+	void freemem() {
+		particles.dlNode(particles.get_head());
+	}
 };
 
 void carsoneffect();
